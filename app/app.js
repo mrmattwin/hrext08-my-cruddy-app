13001067e71d5dd98eeced93442874375cf2897c
David Berry <david@davidberry.io>
2019-03-29 09:29:54 -0700

    pseudocode complete

--- app/app.js
+++ app/app.js
@@ -1,6 +1,12 @@
-/*
-Init app
-interact with DOM
-interact with localstorage
+ //jQuery document ready initialization stuff
+ ////button and form event handlers
+ // logic for determining action probably needs to go in the event handler
 
- */
+
+ //localStorage stuff
+ ////create new entry
+
+ ////Update existing entry
+
+ ////delete existing entry
+
