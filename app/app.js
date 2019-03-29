c33a3936ef5e6f6efa2c54adb68842e305f21925
David Berry <david@davidberry.io>
2019-03-29 10:28:07 -0700

    localStorage helper functions

--- app/app.js
+++ app/app.js
@@ -1,12 +1,33 @@
  //jQuery document ready initialization stuff
  ////button and form event handlers
  // logic for determining action probably needs to go in the event handler
+/*
 
 
- //localStorage stuff
- ////create new entry
 
- ////Update existing entry
+When an input element is given a name, that name becomes a property of the owning form element's HTMLFormElement.elements property. That means if you have an input whose name is set to guest and another whose name is hat-size, the following code can be used:
 
- ////delete existing entry
+let form = document.querySelector("form");
+let guestName = form.elements.guest;
+let hatSize = form.elements["hat-size"];
+*/
 
+//localStorage stuff
+//https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+////create new entry
+//localStorage.setItem(key, value)
+var createEntry = function(key, value) {
+	return localStorage.setItem(key, value);
+}
+
+////Update existing entry
+//localStorage.setItem(key, value)
+var updateEntry = function(key, value) {
+	return localStorage.setItem(key, value);
+}
+
+////delete existing entry
+//localStorage.removeItem(key)
+var removeEntry = function(key) {
+	return localStorage.removeItem(key);
+}
