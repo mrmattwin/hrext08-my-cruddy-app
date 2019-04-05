ac20222eb4dc647c2fadcf294114bf9848b0b98d
Matthew Nguyen <mattnguyen@Matthews-MBP.attlocal.net>
2019-04-04 21:51:59 -0700

    Completed my cruddy app

--- app/app.js
+++ app/app.js
@@ -8,8 +8,19 @@ var loadLocalStorage = function () {
 };
 
 var updateStatusLabel = function(message) {
-	$('#statusLabel').text('Status: ' + message);
+	$('#statusLabel').text(message);
 }
+
+var updateBudgetStatus = function(message) {
+	$('#statusBudget').text(message);
+}
+
+var chartData = Object.keys(localStorage).map(function(key) {
+   return [key, Number(localStorage[key])];
+});
+
+
+var budgetAmount;
 
  //jQuery document ready initialization stuff
  ////button and form event handlers
@@ -23,15 +34,27 @@ $(document).ready(function () {
 		var keyExists = localStorage.getItem(key) !== null;
 
 		if (keyExists) {
-			updateStatusLabel('key already exists, please use update button instead! :D');
+			updateStatusLabel('Expense already exists, use update button instead!');
 		} else if (key === '') {
-			updateStatusLabel('invalid input!')
+			updateStatusLabel('Invalid input!')
+		}else if (isNaN(Number(value)) || value === '') {
+			updateStatusLabel('Amount must be a number!')
 		}else {
 			createEntry(key, value);
-			updateStatusLabel('key created - ' + key);
+			updateStatusLabel('Expense added - ' + key);
+			document.getElementById('key').value='';
+			document.getElementById('value').value='';
 		}
 
 		loadLocalStorage();
+		chartData = Object.keys(localStorage).map(function(key) {
+		   return [key, Number(localStorage[key])];
+		});
+		checkTotalBudget();
+		chart.load({
+        columns: chartData
+   		});
+
 	});
 
 	$('#btn-update').on('click', function(e) {
@@ -41,17 +64,28 @@ $(document).ready(function () {
 		var keyExists = existingValue !== null;
 
 		if (value === existingValue) {
-			updateStatusLabel('key not updated - that value already exists silly! xD')
+			updateStatusLabel('Expense not updated - that value already exists!')
+		} else if (isNaN(Number(value))) {
+			updateStatusLabel('Amount must be a number!')
 		} else if (keyExists) {
 			updateEntry(key, value);
-			updateStatusLabel('key updated - ' + key);
+			updateStatusLabel('Expense updated - ' + key);
+			document.getElementById('key').value='';
+			document.getElementById('value').value='';
 		} else if (key === '') {
-			updateStatusLabel('invalid input!')
+			updateStatusLabel('Invalid input!')
 		} else {
-			updateStatusLabel('key doesn\'t exist, please use create button instead! :D');
+			updateStatusLabel('Expense doesn\'t exist, use create button instead!');
 		}		
 		
-		loadLocalStorage();		
+		loadLocalStorage();	
+		chartData = Object.keys(localStorage).map(function(key) {
+		   return [key, Number(localStorage[key])];
+		});
+		checkTotalBudget();
+		chart.load({
+        columns: chartData
+   		});
 	});
 
 	$('#btn-delete').on('click', function(e) {
@@ -61,21 +95,76 @@ $(document).ready(function () {
 
 		if (keyExists) {
 			removeEntry(key);
-			updateStatusLabel('key removed - ' + key);
+			updateStatusLabel('Expense removed - ' + key);
+			document.getElementById('key').value='';
+			document.getElementById('value').value='';
 		} else if (key === '') {
-			updateStatusLabel('invalid input!')
+			updateStatusLabel('Invalid input!')
 		} else {
-			updateStatusLabel('key doesn\'t exist, nothing removed. :|');
+			updateStatusLabel('Expense doesn\'t exist, nothing removed.');
 		}
 
 		loadLocalStorage();
+		chartData = Object.keys(localStorage).map(function(key) {
+		   return [key, Number(localStorage[key])];
+		});
+		checkTotalBudget();
+		chart.load({
+		unload: true,
+        columns: chartData
+   		});
+	});
+
+	$('#btn-budget').on('click', function(e){
+		var sum = 0;
+		chartData.forEach(function(element){
+			sum += element[1];
+		});
+		budgetAmount = Number($('#totalBudget').val());
+
+		if(sum > 0) {
+			checkTotalBudget();
+		} 
+
+		if(isNaN(Number(budgetAmount))) {
+			updateBudgetStatus(`Must enter a number!`);
+		} else if(budgetAmount === 0) {
+			updateBudgetStatus(`Must enter an amount!`);
+		} else if (sum > 0) {
+			checkTotalBudget();
+		} else {
+			updateBudgetStatus(`Total budget set to $${budgetAmount}`);
+		}
+
+		
 	});	
-
 });
+
+var chart = c3.generate({
+    data: {
+        columns: chartData,  
+        type : 'pie',
+        empty: { label: { text: "No Data Available" }   }
+    },
+    tooltip: { 
+    	format: { 
+    		value: function (x) { 
+				var format = d3.format('$,');
+    			return format(x); } } },
+	// pie: {
+ //        label: {
+ //            format: function (value, ratio, id) {
+ //                return d3.format('$,')(value);
+ //            }
+ //        }
+ //    }
+});
+
+
+// d3.selectAll(".c3-legend-item-chartData text").text(chartData)
+
+
 /*
-
-
-
 When an input element is given a name, that name becomes a property of the owning form element's HTMLFormElement.elements property. That means if you have an input whose name is set to guest and another whose name is hat-size, the following code can be used:
 
 let form = document.querySelector("form");
@@ -107,3 +196,22 @@ var updateEntry = function(key, value) {
 var removeEntry = function(key) {
 	return localStorage.removeItem(key);
 }
+
+var checkTotalBudget = function() {
+	var sum = 0;
+	chartData.forEach(function(element){
+		sum += element[1];
+	});
+	if(!budgetAmount) {
+		updateBudgetStatus(`Total budget not set yet!`);
+	} else if(sum > budgetAmount) {
+		updateBudgetStatus(`Overbudget by $${-(budgetAmount - sum)}!`);
+	} else if(sum < budgetAmount) {
+		updateBudgetStatus(`$${(budgetAmount - sum)} left in budget`);
+	} else if (sum === budgetAmount) {
+		updateBudgetStatus(`BUDGET MAXED`);
+	} 
+}
+
+
+
